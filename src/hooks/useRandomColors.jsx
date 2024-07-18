b48c5dd3ac87a79bb975b895fc42e67271c78174
Yara Higagy <yara.higagy@gmail.com>
2024-07-18 20:39:44 +0300

    more colors

--- src/hooks/useRandomColors.jsx
+++ src/hooks/useRandomColors.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react';
 function useRandomColors() {
     const [color, setColor] = useState('');
 
-    const colors = ['teal', 'tomato', 'olive', 'rose', 'brown', 'purple'];
+    const colors = ['teal', 'tomato', 'olive', 'rose', 'brown', 'purple', 'beige', 'chocolate', 'darkgreen', 'midnightblue'];
 
     useEffect(() => {
         // Initialize with a random color on first render
